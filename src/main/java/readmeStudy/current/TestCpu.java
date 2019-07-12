671e615e2ed9774821c07b67a55eb4e4f39ad065
ybl <1162474984@qq.com>
2019-07-12 16:48:57 +0800

    socket

--- src/main/java/readmeStudy/current/TestCpu.java
+++ src/main/java/readmeStudy/current/TestCpu.java
@@ -7,22 +7,23 @@ package readmeStudy.current;
  */
 public class TestCpu {
 
-  public static void main(String[] args)throws InterruptedException {
+  public static void main(String[] args) throws InterruptedException {
     test_Thread();
   }
-  public static void  test_Thread()throws InterruptedException {
-    while (true) {
-    for (int i = 0; i < Integer.MAX_VALUE; i++) {
-    Runnable runnable = () -> {
-    while (true)
-    try {
-    Thread.sleep(10);
-    } catch (InterruptedException e) {
 
-    }
-    new Thread(runnable).start();
-    };
-    }
+  public static void test_Thread() throws InterruptedException {
+    while (true) {
+      for (int i = 0; i < Integer.MAX_VALUE; i++) {
+        Runnable runnable = () -> {
+          while (true) {
+            try {
+              Thread.sleep(10);
+            } catch (InterruptedException e) {
+
+            }
+          }
+        };
+      }
     }
   }
 }
