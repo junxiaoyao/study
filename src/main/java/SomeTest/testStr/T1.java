e7c7f13713923ee142e70dc63abb78b3bf7482a6
ybl <1162474984@qq.com>
2020-03-23 16:59:02 +0800

    s.intern()笔记

--- src/main/java/SomeTest/testStr/T1.java
+++ src/main/java/SomeTest/testStr/T1.java
@@ -7,12 +7,15 @@ package SomeTest.testStr;
  */
 public class T1 {
 
-  public static void main(String[] args) {
-    String s=new String(new char[]{'1','1'});
-    s.intern();
-  //  String s1="11";
-    System.out.println(s=="11");
+    public static void main(String[] args) {
+        String s = new String(new char[] {'1', '1'});
+        // 首先查找静态区是否存在该常量，
+      // 如果存在返回地址，
+      // 否则在静态区新建一个常量并返回
+        s.intern();
+        // String s1="11";
+        System.out.println(s == "11");
 
-    System.out.println(s=="11");
-  }
+        System.out.println(s == "11");
+    }
 }
